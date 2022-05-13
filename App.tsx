8305b6a0eb05ff05366fe01a89c9581d9a09a32b
nick-cheatwood7 <nick.cheatwood@gmail.com>
2022-05-13 18:44:09 -0500

    added icons to tab control

--- App.tsx
+++ App.tsx
@@ -1,12 +1,42 @@
 import React from 'react';
-import {NativeBaseProvider, Box} from 'native-base';
+import {Icon, NativeBaseProvider} from 'native-base';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicon from 'react-native-vector-icons/Ionicons';
+
+// Import screens
+import HomeScreen from './src/views/HomeScreen';
+import InfoScreen from './src/views/InfoScreen';
+
+const Tab = createBottomTabNavigator();
 
 const App: React.FC = () => {
   return (
     <NativeBaseProvider>
-      <Box flex={1} alignItems={'center'} justifyContent="center">
-        Hello world
-      </Box>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({route}) => ({
+            tabBarIcon: ({focused, color, size}) => {
+              let iconName: typeof Icon.name = '';
+              if (route.name === 'Home') {
+                iconName = focused ? 'home' : 'home-outline';
+              } else if (route.name === 'Info') {
+                iconName = focused
+                  ? 'information-circle'
+                  : 'information-circle-outline';
+              }
+              // Render the icon
+              return (
+                <Icon as={Ionicon} name={iconName} color={color} size={size} />
+              );
+            },
+            tabBarActiveTintColor: 'blue',
+            tabBarInactiveTintColor: 'gray',
+          })}>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Info" component={InfoScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </NativeBaseProvider>
   );
 };
