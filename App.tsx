a4a6554138d1480cb6d4de864742f87c340c31f7
nick-cheatwood7 <nick.cheatwood@gmail.com>
2022-05-14 10:06:38 -0500

    added redux

--- App.tsx
+++ App.tsx
@@ -5,6 +5,10 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Ionicon from 'react-native-vector-icons/Ionicons';
 import {Button} from './src/constants/theme';
 
+// Redux stuff
+import {store} from './src/redux/store';
+import {Provider} from 'react-redux';
+
 // Import screens
 import {RootTabParamList} from './src/routes/RootTabParamList';
 import HomeScreen from './src/views/HomeScreen';
@@ -14,32 +18,39 @@ const Tab = createBottomTabNavigator<RootTabParamList>();
 
 const App: React.FC = () => {
   return (
-    <NativeBaseProvider>
-      <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={({route}) => ({
-            tabBarIcon: ({focused, color, size}) => {
-              let iconName = '';
-              if (route.name === 'Home') {
-                iconName = focused ? 'home' : 'home-outline';
-              } else if (route.name === 'Info') {
-                iconName = focused
-                  ? 'information-circle'
-                  : 'information-circle-outline';
-              }
-              // Render the icon
-              return (
-                <Icon as={Ionicon} name={iconName} color={color} size={size} />
-              );
-            },
-            tabBarActiveTintColor: Button.Primary,
-            tabBarInactiveTintColor: Button.Muted,
-          })}>
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="Info" component={InfoScreen} />
-        </Tab.Navigator>
-      </NavigationContainer>
-    </NativeBaseProvider>
+    <Provider store={store}>
+      <NativeBaseProvider>
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={({route}) => ({
+              tabBarIcon: ({focused, color, size}) => {
+                let iconName = '';
+                if (route.name === 'Home') {
+                  iconName = focused ? 'home' : 'home-outline';
+                } else if (route.name === 'Info') {
+                  iconName = focused
+                    ? 'information-circle'
+                    : 'information-circle-outline';
+                }
+                // Render the icon
+                return (
+                  <Icon
+                    as={Ionicon}
+                    name={iconName}
+                    color={color}
+                    size={size}
+                  />
+                );
+              },
+              tabBarActiveTintColor: Button.Primary,
+              tabBarInactiveTintColor: Button.Muted,
+            })}>
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Info" component={InfoScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </NativeBaseProvider>
+    </Provider>
   );
 };
 
--- src/views/HomeScreen/index.tsx
+++ src/views/HomeScreen/index.tsx
@@ -1,16 +1,48 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Box, Center, Fab, Icon, useToast} from 'native-base';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Map from '../../components/Map';
 import {Button} from '../../constants/theme';
 
+// Redux
+import {useAppDispatch, useAppSelector} from '../../redux/hooks';
+import {setOrigin} from '../../redux/reducers/locationSlice';
+
 const Home: React.FC = () => {
   const toast = useToast();
+  const origin = useAppSelector(state => state.location.origin);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    setCurrentLocation();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // Fetch and set user's current location
+  const setCurrentLocation = async (): Promise<void> => {
+    // Get current location
+    const current = {latitude: 123, longitude: 456};
+    // Set current location in Redux
+    dispatch(
+      setOrigin({latitude: current.latitude, longitude: current.longitude}),
+    );
+  };
+
+  const recenterMap = async (): Promise<void> => {
+    // Alert user location is being fetched
+    toast.show({
+      title: 'Fetching current location',
+      placement: 'top',
+    });
+    // Recenter map
+    await setCurrentLocation();
+  };
+
   return (
     <Center flex={1} ml={0} mr={0}>
       <Box flex={1} alignItems={'center'} justifyContent="center">
         {/* Render the map */}
-        <Map />
+        <Map latitude={origin?.latitude} longitude={origin?.longitude} />
         {/* Render a FAB */}
         <Fab
           renderInPortal={false}
@@ -20,12 +52,7 @@ const Home: React.FC = () => {
           icon={
             <Icon color="white" as={Ionicons} name="locate-outline" size={6} />
           }
-          onPress={() =>
-            toast.show({
-              title: 'Fetching current location',
-              placement: 'top',
-            })
-          }
+          onPress={() => recenterMap()}
         />
       </Box>
     </Center>
